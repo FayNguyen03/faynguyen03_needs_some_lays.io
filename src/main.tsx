5bd5e2c004dfc08c28ecc0b2583ac6ba98b26627
FayNguyen03 <ntkhanh391914@gmail.com>
2025-12-26 03:33:12 -0600

    Fix to deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './Components/App.tsx'
 
+//use HashRouter to avoid issues with the server with GitHub pages
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
