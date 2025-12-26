4c6db91f192d236d39491478eef6d15fca78f8b1
FayNguyen03 <ntkhanh391914@gmail.com>
2025-12-26 02:39:52 -0600

    Remove commas

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/faynguyen03_needs_some_lays.io'
+  base: '/faynguyen03.github.io/'
 })
